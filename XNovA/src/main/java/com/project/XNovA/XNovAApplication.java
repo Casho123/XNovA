5c3a05cc9fc6773c05f48576559e6bc14afbe3a4
My PC <alex.petkov606@gmail.com>
2023-03-26 21:04:46 +0300

    Initial commit

---
+++ XNovA/src/main/java/com/project/XNovA/XNovAApplication.java
@@ -0,0 +1,13 @@
+package com.project.XNovA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XNovAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XNovAApplication.class, args);
+	}
+
+}
---
+++ XNovA/src/test/java/com/project/XNovA/XNovAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.XNovA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XNovAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
